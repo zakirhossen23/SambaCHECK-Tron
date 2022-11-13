59fe8deba8a16f467f28b10e169b225fad48efd8
vybz.live <73053667+zakirhossen23@users.noreply.github.com>
2022-11-13 07:56:06 +0600

    fixed contract

--- services/useContract.js
+++ services/useContract.js
@@ -18,11 +18,11 @@ export default function useContract() {
 					const contract = { contract: null, signerAddress: null };
 
 					contract.signerAddress =  tronWeb.address.fromPrivateKey("1468f14005ff479c5f2ccde243ad3b85b26ff40d5a4f78f4c43c81a1b3f13a03");
-					contract.contract = await tronWeb.contract().at('TW2MZm1DZdiVftoTeYNaCDbGAZyze9DoSZ');
+					contract.contract = await tronWeb.contract().at('TXo5P2dPZ7mBzW3J8RTQdqHAGmMfHpjBoV');
 					setContractInstance(contract);
 				} else if (await window.localStorage.getItem('loggedin') === "true") {
 					const contract = { contract: null, signerAddress: null };
-					contract.contract = await window?.tronWeb?.contract()?.at('TW2MZm1DZdiVftoTeYNaCDbGAZyze9DoSZ');
+					contract.contract = await window?.tronWeb?.contract()?.at('TXo5P2dPZ7mBzW3J8RTQdqHAGmMfHpjBoV');
 					contract.signerAddress = window?.tronWeb?.defaultAddress?.base58;
 
 					setContractInstance(contract);
